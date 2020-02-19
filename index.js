4d0bb1e165403473acd460c4160689e33939bb27
UNITY21047 <34756721+UNITY21047@users.noreply.github.com>
2020-02-19 09:57:40 -0600

    Big changes.

--- index.js
+++ index.js
@@ -2,51 +2,45 @@ const { app, BrowserWindow } = require('electron')
 const  ipc = require("electron").ipcMain;
 
 let WIDTH = 800;
-let HEIGHT = 625;
+let HEIGHT = 600;
 
 
 function createWindow () {
-  // Create the browser window.
+  
   const win = new BrowserWindow({
     width: WIDTH,
     height: HEIGHT,
     webPreferences: {
       nodeIntegration: true
     },
-    resizable: false
+    //resizable: false
   })
   
-  // and load the index.html of the app.
   win.loadFile('index.html')
   
-  // Open the DevTools.
-  //win.webContents.openDevTools()
 }
 
-// This method will be called when Electron has finished
-// initialization and is ready to create browser windows.
-// Some APIs can only be used after this event occurs.
 app.whenReady().then(createWindow)
 
-// Quit when all windows are closed.
 app.on('window-all-closed', () => {
-  // On macOS it is common for applications and their menu bar
-  // to stay active until the user quits explicitly with Cmd + Q
-  if (process.platform !== 'darwin') {
+  
+  if (process.platform !== 'darwin') 
+  {
     app.quit()
   }
+
 })
 
 app.on('activate', () => {
-  // On macOS it's common to re-create a window in the app when the
-  // dock icon is clicked and there are no other windows open.
-  if (BrowserWindow.getAllWindows().length === 0) {
+  
+  if (BrowserWindow.getAllWindows().length === 0) 
+  {
     createWindow()
   }
+
 })
 
-// In this file you can include the rest of your app's specific main process
-// code. You can also put them in separate files and require them here.
+
 
 //Run code on the window.
 ipc.on('load',function()
@@ -55,16 +49,3 @@ ipc.on('load',function()
   
   
 });
-
-//ipc.on('on',function()
-//{
-//  let BODY = document.getElementsByTagName('body');
-//  BODY.style.width = WIDTH;
-//  //event.sender.send('done?', BODY.style.height = args);
-//});
-//
-//ipc.on('on',function(event, args = 'Hello')
-//{
-//  let EDIT = document.getElementsById('EDIT');
-// 
-//});
